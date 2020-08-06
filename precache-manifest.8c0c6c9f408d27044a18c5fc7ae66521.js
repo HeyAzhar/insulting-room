7502a3ddd5e614d080a0869c20445d0e5c3d6307
HeyAzhar <sk.azharuddin1230@gmail.com>
2020-08-07 02:57:48 +0530

    Updates

---
+++ precache-manifest.8c0c6c9f408d27044a18c5fc7ae66521.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26644dcc8a4610488e2231e4e50bce35",
+    "url": "/insulting-room/index.html"
+  },
+  {
+    "revision": "fd5faacb12988bfe5f69",
+    "url": "/insulting-room/static/css/2.e7b3b84c.chunk.css"
+  },
+  {
+    "revision": "b58ddcdb5eb59b704411",
+    "url": "/insulting-room/static/css/main.615da19e.chunk.css"
+  },
+  {
+    "revision": "fd5faacb12988bfe5f69",
+    "url": "/insulting-room/static/js/2.a1d32eab.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/insulting-room/static/js/2.a1d32eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b58ddcdb5eb59b704411",
+    "url": "/insulting-room/static/js/main.8efbdb73.chunk.js"
+  },
+  {
+    "revision": "46f54bf6e535ee5b5498",
+    "url": "/insulting-room/static/js/runtime-main.b2861821.js"
+  }
+]);
